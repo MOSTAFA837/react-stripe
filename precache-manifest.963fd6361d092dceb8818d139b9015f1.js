bf8e9eff8be8b895eedbbd124858c4dc8fdb5d9a
MOSTAFA837 <mostafamanchester837@gmail.com>
2021-12-15 13:52:29 +0200

    Updates

---
+++ precache-manifest.963fd6361d092dceb8818d139b9015f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6530471075d6f739189d950059927c6",
+    "url": "/stripe/index.html"
+  },
+  {
+    "revision": "5d390c8c61e8f442fa2a",
+    "url": "/stripe/static/css/main.ccbc3dbe.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d390c8c61e8f442fa2a",
+    "url": "/stripe/static/js/main.917a8f7e.chunk.js"
+  },
+  {
+    "revision": "f25d2cafeeda02dc947e",
+    "url": "/stripe/static/js/runtime-main.c732f43d.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/stripe/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/stripe/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/stripe/static/media/phone.e908706c.svg"
+  }
+]);
